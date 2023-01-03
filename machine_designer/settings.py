191a5531f4b04e89dd24ce73ec0fafda0ee3cb24
MoonSold <55290172+MoonSold@users.noreply.github.com>
2023-01-03 13:27:53 +0300

    Update settings.py

--- machine_designer/settings.py
+++ machine_designer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ibl6k$unzih-@6kz^$2_d3aw)-cy66cq!kmrr0t0y$h2=79to8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
